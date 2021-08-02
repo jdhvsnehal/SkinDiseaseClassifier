35ad3c1d1978dd07e9b8e70f7323897d25259bc4
jdhvsnehal <jdhvsnehal1@gmail.com>
2021-08-02 16:31:10 +0530

    commit 2

---
+++ app.py
@@ -0,0 +1,104 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import keras
+from keras.utils.np_utils import to_categorical
+from keras.models import Sequential, load_model
+from keras import backend as K
+import os
+import time
+import io
+from PIL import Image
+
+
+MODELSPATH = './models/'
+
+
+def render_header():
+    st.write("""
+        <p align="center"> 
+            <H1> Dermatology Classification  
+            <H2> Skin Cancer Prediction 
+        </p>
+
+    """, unsafe_allow_html=True)
+
+
+@st.cache
+def data_gen(x):
+    img = np.asarray(Image.open(x).resize((100, 75)))
+    x_test = np.asarray(img.tolist())
+    x_test_mean = np.mean(x_test)
+    x_test_std = np.std(x_test)
+    x_test = (x_test - x_test_mean) / x_test_std
+    x_validate = x_test.reshape(1, 75, 100, 3)
+
+    return x_validate
+
+
+@st.cache
+def data_gen_(img):
+    img = img.reshape(100, 75)
+    x_test = np.asarray(img.tolist())
+    x_test_mean = np.mean(x_test)
+    x_test_std = np.std(x_test)
+    x_test = (x_test - x_test_mean) / x_test_std
+    x_validate = x_test.reshape(1, 75, 100, 3)
+
+    return x_validate
+
+
+def load_models():
+
+    model = load_model(MODELSPATH + 'model.h5')
+    return model
+
+
+def predict(x_test, model):
+    Y_pred = model.predict(x_test)
+    ynew = model.predict_proba(x_test)
+    K.clear_session()
+    ynew = np.round(ynew, 2)
+    ynew = ynew*100
+    y_new = ynew[0].tolist()
+    Y_pred_classes = np.argmax(Y_pred, axis=1)
+    K.clear_session()
+    return y_new, Y_pred_classes
+
+
+@st.cache
+def display_prediction(y_new):
+    """Display image and preditions from model"""
+
+    result = pd.DataFrame({'Probability': y_new}, index=np.arange(7))
+    result = result.reset_index()
+    result.columns = ['Classes', 'Probability']
+    lesion_type_dict = {2: 'Benign keratosis-like lesions', 4: 'Melanocytic nevi', 3: 'Dermatofibroma',
+                        5: 'Melanoma', 6: 'Vascular lesions', 1: 'Basal cell carcinoma', 0: 'Actinic keratoses'}
+    result["Classes"] = result["Classes"].map(lesion_type_dict)
+    return result
+
+
+def main():
+        st.header("Dermatology Classification")
+        st.subheader("--Skin Cancer Prediction--")
+
+        file_path = st.file_uploader('Upload an image', type=['png', 'jpg'])
+
+        if file_path is not None:
+            x_test = data_gen(file_path)
+            image = Image.open(file_path)
+            img_array = np.array(image)
+
+            st.image(img_array, caption='Uploaded Image',
+                     use_column_width=True)
+
+            if st.button('Predict'):
+                model = load_models()
+
+                y_new, Y_pred_classes = predict(x_test, model)
+                result = display_prediction(y_new)
+                st.write(result)
+
+if __name__ == "__main__":
+    main()
